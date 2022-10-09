0d30d6081b79771035076a57fa6f2eb3cc42c767
stevenXS <1165160978@qq.com>
2022-10-09 22:50:07 +0800

    fragment:增加fragment的基本用法

---
+++ fragment_demo/src/main/java/com/steven/fragment_demo/LeftFragment.java
@@ -0,0 +1,24 @@
+package com.steven.fragment_demo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LeftFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_left, container, false);
+    }
+}
