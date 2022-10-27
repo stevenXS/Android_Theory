31d8615be9ad7b8973cf41fae31951120b92f908
xiongshuqi <xiongshuqi@bytedance.com>
2022-10-27 21:02:11 +0800

    multimedia:增加将拍照存储到相册的功能，有bug

--- multi_media_demo/src/main/java/com/steven/multi_media_demo/CameraMainActivity.java
+++ multi_media_demo/src/main/java/com/steven/multi_media_demo/CameraMainActivity.java
@@ -1,19 +1,32 @@
 package com.steven.multi_media_demo;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.ActivityManager;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,6 +34,8 @@ import java.io.IOException;
 
 public class CameraMainActivity extends AppCompatActivity {
     public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO = 2;
+    public static final int REQUEST_CODE = 20;
     private Uri imageUri;
     private ImageView picture;
 
@@ -29,6 +44,7 @@ public class CameraMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button open = findViewById(R.id.open_camera);
+        Button choose_photo = findViewById(R.id.choose_photo);
         picture = findViewById(R.id.show_picture);
         open.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +70,41 @@ public class CameraMainActivity extends AppCompatActivity {
             }
         });
 
+        choose_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(CameraMainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                  != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(CameraMainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+                }else {
+                    openAlbum();
+                }
+            }
+        });
+
+    }
+
+    // open the album
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_CODE:
+                if (permissions.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                }else {
+                    Toast.makeText(CameraMainActivity.this, "permission deined.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -69,8 +120,68 @@ public class CameraMainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_CANCELED){
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+                        handleImageOnKitKat(data);
+                    }else {
+                        handleImageBeforeKitKot(data);
+                    }
+                }
             default:
                 break;
         }
+    }
+
+    private void handleImageBeforeKitKot(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, uri)){
+            // if document is Uri, get it by document id.
+            String documentId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())){
+                // verify package name for provider.
+                String id = documentId.split(":")[1];
+                Log.d("photo", "id is " + id);
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                Log.d("photo", "selection is " + selection);
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            }else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(documentId));
+                imagePath = getImagePath(contentUri, null);
+                Log.d("photo", "path is " + imagePath);
+            }
+        }else if ("content".equalsIgnoreCase(uri.getScheme())){
+            imagePath = getImagePath(uri, null);
+        }else if ("file".equalsIgnoreCase(uri.getScheme())){
+            imagePath = getImagePath(uri, null);
+        }
+        displayImage(imagePath);
+    }
+
+    private void displayImage(String imgPath) {
+        if (!TextUtils.isEmpty(imgPath)){
+            Bitmap bitmap = BitmapFactory.decodeFile(imgPath);
+            picture.setImageBitmap(bitmap);
+        }
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null){
+            while (cursor.moveToNext()){
+                path = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
     }
 }
