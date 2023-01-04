b41958d4caa66c1a2d87b0b4d5349993f345b0c2
xiongshuqi <xiongshuqi@bytedance.com>
2023-01-04 17:47:38 +0800

    feature: 增加新的module
    影响范围：无

---
+++ multi_flavor_demo_test/src/main/java/com/steven/multi_flavor_demo_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.steven.multi_flavor_demo_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
