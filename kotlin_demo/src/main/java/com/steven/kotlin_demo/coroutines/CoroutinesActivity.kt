9c7e921dc633e7f6d70fa7f9bc2daa4fb270066c
stevenXS <1165160978@qq.com>
2023-02-05 11:03:01 +0800

    kotlin: 学习协程的基本使用

---
+++ kotlin_demo/src/main/java/com/steven/kotlin_demo/coroutines/CoroutinesActivity.kt
@@ -0,0 +1,12 @@
+package com.steven.kotlin_demo.coroutines
+
+import android.os.Bundle
+import androidx.activity.R
+import androidx.appcompat.app.AppCompatActivity
+
+class CoroutinesActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.notification_action)
+    }
+}
