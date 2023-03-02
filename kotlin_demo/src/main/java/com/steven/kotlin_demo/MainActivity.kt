1564a029dcf0297263cb58a65a0fb628450d545b
StevenXS <1165160978@qq.com>
2023-03-02 22:30:12 +0800

    feat: 基于泛型方法实现创建多个View子类的视图

--- kotlin_demo/src/main/java/com/steven/kotlin_demo/MainActivity.kt
+++ kotlin_demo/src/main/java/com/steven/kotlin_demo/MainActivity.kt
@@ -2,6 +2,10 @@ package com.steven.kotlin_demo
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Switch
+import android.widget.TextView
 import android.widget.Toast
 import com.steven.kotlin_demo.commonActivity.CommonActivity
 import com.steven.kotlin_demo.coroutines.CoroutinesActivity
@@ -10,7 +14,7 @@ import com.steven.kotlin_demo.sqllite.SQLLiteActivity
 class MainActivity : CommonActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        // 非泛型方法
         addBtn("测试按钮", ""){
             Toast.makeText(this, "aaaa", Toast.LENGTH_SHORT).show()
         }
@@ -20,6 +24,19 @@ class MainActivity : CommonActivity() {
         }
 
         addBtn("SQLite", ""){
+            startActivity(Intent(this, SQLLiteActivity::class.java))
+        }
+
+        // 泛型方法
+        addView("Text", "", TextView(this)){
+            startActivity(Intent(this, SQLLiteActivity::class.java))
+        }
+
+        addView("Switch", "", Switch(this)){
+            startActivity(Intent(this, SQLLiteActivity::class.java))
+        }
+
+        addView("Button", "", Button(this)){
             startActivity(Intent(this, SQLLiteActivity::class.java))
         }
     }
--- kotlin_demo/src/main/java/com/steven/kotlin_demo/commonActivity/CommonActivity.kt
+++ kotlin_demo/src/main/java/com/steven/kotlin_demo/commonActivity/CommonActivity.kt
@@ -7,6 +7,7 @@ import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Switch
 import android.widget.TextView
 import com.steven.kotlin_demo.R
 import kotlinx.android.synthetic.main.root_layout.*
@@ -41,5 +42,34 @@ open class CommonActivity: Activity() {
         }
 
         return button;
+    }
+
+    fun <T: View>addView(name: String, desc: String, view: T, onClick: ()->Unit ): T {
+        if (view is Button){
+            view.text = name
+        }else if(view is Switch){
+            view.text = name
+        }else if(view is TextView){
+            view.text = name
+        }
+        view.setOnClickListener {
+            onClick.invoke()
+        }
+        val lp = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        ll_root!!.addView(view, lp)
+
+        if (!TextUtils.isEmpty(desc)){
+            val descTv = TextView(this)
+            descTv.text = desc
+            descTv.gravity = Gravity.CENTER
+            ll_root!!.addView(descTv, lp)
+            ll_root!!.addView(View(this), LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,12))
+        }
+
+        return view;
     }
 }
