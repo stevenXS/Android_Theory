52d2186aafd13863ca843a1c527b70ae6a18637c
StevenXS <1165160978@qq.com>
2023-06-03 11:07:44 +0800

    [view] 实现View展示倒计时

--- test_demo/src/main/java/com/steven/test_demo/activity/CustomLoadingActivity.java
+++ test_demo/src/main/java/com/steven/test_demo/activity/CustomLoadingActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.steven.test_demo.R;
 import com.steven.test_demo.custom_view.CustomLoadingView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class CustomLoadingActivity extends AppCompatActivity {
     private CustomLoadingView customLoading;
+    private int time = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +24,35 @@ public class CustomLoadingActivity extends AppCompatActivity {
         customLoading = findViewById(R.id.custom_loading);
         Button start = findViewById(R.id.bt_start);
         Button stop = findViewById(R.id.bt_stop);
+        TextView show_countdown = findViewById(R.id.show_countdown);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 customLoading.start();
             }
         });
-
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 customLoading.stop();
             }
         });
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        show_countdown.setText("倒计时：" + time);
+                        time--;
+                        if (time < 0) {
+                            timer.cancel();
+                            show_countdown.setText("倒计时结束");
+                        }
+                    }
+                });
+            }
+        },0,1000);
     }
 }
