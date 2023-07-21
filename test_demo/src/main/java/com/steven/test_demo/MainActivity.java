9d15560c807c5472182df3cfb30064f0c7d71589
StevenXS <1165160978@qq.com>
2023-07-21 22:14:37 +0800

    [view] 自定义FloatButton，了解view的布局参数、起始坐标的使用和设置

--- test_demo/src/main/java/com/steven/test_demo/MainActivity.java
+++ test_demo/src/main/java/com/steven/test_demo/MainActivity.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.steven.test_demo.activity.CountDownActivity;
 import com.steven.test_demo.activity.CustomLoadingActivity;
 import com.steven.test_demo.activity.RemoteAMainActivity;
 import com.steven.test_demo.activity.RemoteBMainActivity;
 import com.steven.test_demo.activity.WebViewActivity;
+import com.steven.test_demo.custom_view.CustomFloatActionButton;
 
 public class MainActivity extends AppCompatActivity {
     @SuppressLint("MissingInflatedId")
@@ -62,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        addView("FloatButton", new CustomFloatActionButton.FloatButtonListener(this, "测试"),
+                new CustomFloatActionButton(this));
     }
 
     public void addButton(String btnName, View.OnClickListener listener){
@@ -69,11 +75,35 @@ public class MainActivity extends AppCompatActivity {
         Button button = new Button(this);
         button.setText(btnName);
         button.setTag(btnName);
-
-        button.setOnClickListener(listener);
-
+        if (listener != null) {
+            button.setOnClickListener(listener);
+        }
         if (rootView != null){
             rootView.addView(button, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
         }
+    }
+
+    public void addView(String title, View.OnClickListener listener, View view) {
+        LinearLayout rootView = findViewById(R.id.ll_root);
+        rootView.setOrientation(LinearLayout.VERTICAL);
+        if (view == null) {
+            view = new TextView(this);
+            rootView.addView(view);
+        }
+        if (listener != null) {
+            view.setOnClickListener(listener);
+        }
+        // 获取当前屏幕大小
+        WindowManager windowManager = getWindowManager();
+        Display display = windowManager.getDefaultDisplay();
+        int width = display.getWidth();
+
+        // 当前布局中，view的起始位置, 偏移量。当前view相当于该view的布局的偏移量
+        // 布局的左上角为原点
+        view.setX((float) (width * 0.4));
+        view.setY(20); // 相比原点，Y方向下移动N像素
+
+        // LinearLayout.LayoutParams决定view的布局大小
+        rootView.addView(view, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
     }
 }
