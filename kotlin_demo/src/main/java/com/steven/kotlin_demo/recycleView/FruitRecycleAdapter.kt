129aed6d6f4f66a4c82d5f57fc25446a402ac203
StevenXS <1165160978@qq.com>
2023-07-29 12:47:25 +0800

    [ListView] 重写了解使用逻辑：定义ListView控件 + item自定义布局（需要继承adapter）；recycleView和ListView用法类似，但recycleView效率更高，内存可复用；

--- kotlin_demo/src/main/java/com/steven/kotlin_demo/recycleView/FruitRecycleAdapter.kt
+++ kotlin_demo/src/main/java/com/steven/kotlin_demo/recycleView/FruitRecycleAdapter.kt
@@ -12,19 +12,21 @@ import com.steven.kotlin_demo.R
 import com.steven.kotlin_demo.view.Fruit
 
 class FruitRecycleAdapter(val data: List<Fruit>): Adapter<FruitRecycleAdapter.ViewHolder>() {
-
+    // Holder对应Item，item布局可自定义
     inner class ViewHolder(view: View): androidx.recyclerview.widget.RecyclerView.ViewHolder(view){
         val fruitImage: ImageView = view.findViewById(R.id.fruit_image)
         val fruitName: TextView = view.findViewById(R.id.fruit_name)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // 初始化item布局中的属性
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.recycle_view_fruit, parent,false)
         return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
+        // 返回数据的大小
         return data.size
     }
 
